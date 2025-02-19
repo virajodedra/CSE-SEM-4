6de51c795fdbb5fcba3ce0da05b61da541403c95
Odedra Viraj <virajodedra1808@gmail.com>
2025-02-19 11:08:39 +0530

    Add files via upload

---
+++ Python Programming - Lab - 11/calc.py
@@ -0,0 +1,18 @@
+def add(num1, num2) :
+
+    return num1 + num2
+
+def substration(num1, num2) :
+
+    return num1 - num2
+
+def div(num1, num2) :
+    if(num1 == 0) :
+        print("can't divide by Zero !")
+        return;
+
+    return num1 / num2
+
+def multiply(num1, num2) :
+
+    return num1 * num2
